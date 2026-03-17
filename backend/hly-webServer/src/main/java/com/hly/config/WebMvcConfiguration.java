5c2278ebc20819ecd2bcb5a66afb38ab443a8a85
CoMoYo-Yanke <yanke1001@qq.com>
2026-03-17 09:39:41 +0800

    后端-拦截器开发

---
+++ backend/hly-webServer/src/main/java/com/hly/config/WebMvcConfiguration.java
@@ -0,0 +1,30 @@
+package com.hly.config;
+
+import com.hly.interceptor.JwtTokenAdminInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import java.util.List;
+
+/**
+ * 配置类
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfiguration {
+    @Autowired
+    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
+    
+    protected void addInterceptor(InterceptorRegistry registry){
+        log.info("注册自定义拦截器");
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .excludePathPatterns("/api/user/login")
+                .addPathPatterns("/api/**");
+    }
+    
+    
+}
