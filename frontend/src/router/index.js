97c38023393be5338404fbf08053357ba3553d96
CoMoYo-Yanke <yanke1001@qq.com>
2026-03-27 23:48:18 +0800

    前端-基本框架搭建

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/home/Index.vue')
+  },
+  {
+    path: '/user/info',
+    component: () => import('@/views/user/Info.vue')
+  },
+  {
+    path: '/health/record',
+    component: () => import('@/views/health/Record.vue')
+  },
+  {
+    path: '/health/report',
+    component: () => import('@/views/health/Report.vue')
+  },
+  {
+    path: '/diet',
+    component: () => import('@/views/diet/Index.vue')
+  },
+  {
+    path: '/sport',
+    component: () => import('@/views/sport/Index.vue')
+  },
+  {
+    path: '/sleep',
+    component: () => import('@/views/sleep/Index.vue')
+  },
+  {
+    path: '/system/setting',
+    component: () => import('@/views/system/Setting.vue')
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
